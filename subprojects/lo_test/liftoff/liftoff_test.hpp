a773f6c3c415412bb4080823ccadda4adc3bc96e
Jussi Pakkanen <jpakkane@gmail.com>
2017-07-24 19:49:53 +0300

    Created simple test subproject.

---
+++ subprojects/lo_test/liftoff/liftoff_test.hpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2017 Jussi Pakkanen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *    http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#if defined _WIN32 || defined __CYGWIN__
+  #define LIFTOFF_TEST_PUBLIC __declspec(dllexport)
+#else
+  #if defined __GNUC__
+    #define LIFTOFF_TEST_PUBLIC __attribute__ ((visibility("default")))
+  #else
+    #pragma message ("Compiler does not support symbol visibility.")
+    #define LIFTOFF_TEST_PUBLIC
+  #endif
+#endif
+
+namespace liftoff {
+
+void LIFTOFF_TEST_PUBLIC assert_true(bool condition);
+
+}
